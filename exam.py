750bc8ab1344eaf60ce4b2a20d444f439b80c285
viatkinviatkin <svyatkin@skyori.ru>
2021-12-21 22:29:56 +0500

    Добавлена корреляция между уровнем зп и комптенециями

--- exam.py
+++ exam.py
@@ -9,7 +9,7 @@ from collections import Counter
 import itertools
 import re
 
-JOB = 'программист'
+JOB = 'повар'
 QUERY = 'https://perm.hh.ru/search/vacancy?clusters=true&area=72&ored_clusters=true&enable_snippets=true&salary=&text='
 
 def connect(url):
@@ -23,7 +23,7 @@ def connect(url):
     driver.get(url)
     return driver
 
-def barChart(dict1):
+def bar_chart(dict1):
     x = list(dict1.keys())
     y = list(dict1.values())
     color_rectangle = np.random.rand(7, 3)
@@ -34,57 +34,134 @@ def barChart(dict1):
     fig.set_figheight(6)    #  высота "Figure"
     fig.set_facecolor('floralwhite')
     ax.set_facecolor('seashell')
+    ax.set_xticklabels(x, rotation=45)
     jobName = str(f'{JOB}ом')
     plt.title('Наиболее востребованные компетенции\nсреди предложений о работе '+ r"$\bf{" + jobName + "}$\n"+'на сайте hh.ru')
     plt.xlabel('Компетенции')
     plt.ylabel('Востребованность')
     plt.show()
 
-def WriteFile(fileName, freqDictionary):
+def salary_barchart(dict1, dict2):
+
+    x = list(dict1.keys())
+    y1 = list(dict1.values())
+    y2 = list(dict2.values())
+    
+    fig, ax = plt.subplots()
+    color_rectangle = np.random.rand(7, 3)    # RGB
+    
+    ax.bar(x, y1, color = color_rectangle, width = 0.5)
+    color_rectangle = np.random.rand(7, 4)    # RGBA
+    color_rectangle[:,3] = 0.5
+    ax.set_xticklabels(x, rotation=45)
+    ax.bar(x, y2, color = color_rectangle)
+    fig.set_figwidth(12)    #  ширина и
+    fig.set_figheight(6)    #  высота "Figure"
+    fig.set_facecolor('floralwhite')
+    ax.set_facecolor('seashell')
+    ax.set_xticklabels(x, rotation=45)
+    plt.xlabel('Уровень ЗП от наличия компетенций')
+    plt.ylabel('Уровень ЗП в руб.')
+    plt.xlabel('Компетенции')
+    plt.style.use('ggplot')
+    plt.show()
+
+def write_file(fileName, freqDictionary):
     with open(fileName, "w", encoding='utf-8') as output:
         for f in freqDictionary:
             output.write(f + ', ' + str(freqDictionary[f]) + '\n' )
+
+def get_salary(salary_DOM):
+    dirty_salary_value = salary_DOM.get_attribute('textContent')
+    salary = dirty_salary_value.split()
+    salary = ''.join(salary)
+    salary = int(re.findall("\d+",salary)[0])
+
+    if (dirty_salary_value.find('USD')!= -1):
+        print('В элементе',dirty_salary_value,'Нашел usd!')
+        salary *=73
+    if (dirty_salary_value.find('EUR')!= -1):
+        print(dirty_salary_value, 'Нашел eur!')
+        salary *=83
+    
+    return salary
+
 
 
 driver = connect(QUERY+JOB)
 vacancies = []
 for vacancy_DOM_element in driver.find_elements(By.CLASS_NAME,'vacancy-serp-item'):
     vacancies.append(vacancy_DOM_element.find_element(By.CLASS_NAME,'bloko-link').get_attribute('href'))
+for vacancy in vacancies: print(vacancy)
 driver.quit()
 
-skills = []
-skills_salary = {}
-for vacancy in vacancies:
+competencies = []
+skills_salary = []
+for vacancy in vacancies[:8:]:
     driver = connect(vacancy)
-    skills_DOM = [element.get_attribute('textContent') for element in driver.find_elements(By.CLASS_NAME, 'bloko-tag__section.bloko-tag__section_text')]
-    skills.append(skills_DOM)
+    try:
+        skills_select = [element.get_attribute('textContent') for element in driver.find_elements(By.CLASS_NAME, 'bloko-tag__section.bloko-tag__section_text')]
+        competencies.append(skills_select)
 
-    # #Величина зп от компетенций
-    # salary_DOM = driver.find_element(By.CSS_SELECTOR, 'span.bloko-header-2.bloko-header-2_lite')
-    # test = salary_DOM.get_attribute('textContent').split()
-    # test = ''.join(test)
-    # salary = re.findall("\d+",test)[0] 
-    # anonymous_object = type('',(),{'skills':skills_DOM, 'salary':salary})()
-    # print(anonymous_object.salary)
-    # #########################
+        #Уровень ЗП для компетенций
+        salary_DOM = driver.find_element(By.CSS_SELECTOR, 'span.bloko-header-2.bloko-header-2_lite')
+        salary = get_salary(salary_DOM)
 
+        for skill in skills_select:
+            print('skill',skill,'salary', salary)
+            skills_salary_obj = type('',(),{'skill':skill, 'salary':salary})()
+            skills_salary.append(skills_salary_obj)
+    except:
+        print('Не удалось спарсить')
     driver.quit()
-print(skills)
 
-all_skills_together = []
-for microarray in skills:
-    for skill in microarray:
+print('\nКомпетенции\n', competencies)
+
+all_skills = []
+for competency in competencies:
+    for skill in competency:
         if(skill!=''):
             for s in skill.split(','):
-                all_skills_together.append(s) 
+                all_skills.append(s.lower()) 
 
-freq = FreqDist(all_skills_together)
-WriteFile('freqsSkills.txt',freq)
+freq = FreqDist(all_skills)
+write_file('freqsSkills.txt', freq)
 
 sortbyfreqs = {k: v for k, v in sorted(freq.items(), key=lambda item: item[1], reverse=True)}
-barChart(dict(itertools.islice(sortbyfreqs.items(), 10)))
+bar_chart(dict(itertools.islice(sortbyfreqs.items(), 10)))
 
 #Зависимость компетенций от величины зп
+result_avg = []
+result_max = []
+for skills_salary_obj in skills_salary:
+    skill = skills_salary_obj.skill
+
+    if (skill in [obj.skill for obj in result_avg]):
+        continue
+
+    skill_salary_list = [obj.salary for obj in skills_salary if obj.skill == skill]
+
+    max_skill_salary = max(skill_salary_list)
+    result_max.append(type('',(),{'skill':skills_salary_obj.skill, 'max_salary':max_skill_salary})())
+
+    avg_skill_salary = round(sum(skill_salary_list)/len(skill_salary_list))
+    result_avg.append(type('',(),{'skill':skills_salary_obj.skill, 'avg_salary':avg_skill_salary})())
+
+dict_result_avg ={}
+dict_result_max = {}
+
+for obj in result_max: dict_result_max[obj.skill] = obj.max_salary
+for obj in result_avg: dict_result_avg[obj.skill] = obj.avg_salary
+
+salary_barchart(dict(itertools.islice(dict_result_avg.items(), 10)),dict(itertools.islice(dict_result_max.items(), 10)))
+
+##DB
+for obj in result_max:
+    print('skill',obj.skill,'max_salary',obj.max_salary)
+
+for obj in result_avg:
+    print('skill',obj.skill,'avg_salary',obj.avg_salary)
 
 
-
+for row in freq:
+    print('key',freq[row],'value',row)
